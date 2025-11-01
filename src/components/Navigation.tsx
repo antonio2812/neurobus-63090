fe38979341d6a1306a29508816c843a0702a153d
[dyad] <acarlosdesouzajunior@gmail.com>
2025-11-01 13:27:19 -0300

    [dyad] wrote 1 file(s)

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -128,10 +128,11 @@ const Navigation = () => {
 
         {/* 2. Side Panel (Painel Lateral) */}
         <div 
-          className={`lg:hidden fixed top-0 right-0 w-[70%] max-w-xs h-full border-l border-border shadow-2xl z-50 transform transition-transform duration-500 ease-in-out bg-black ${
+          className={`lg:hidden fixed top-0 right-0 w-[70%] max-w-xs h-full border-l border-border shadow-2xl z-50 transform transition-transform duration-500 ease-in-out ${
             isOpen ? 'translate-x-0' : 'translate-x-full'
           }`}
-          // Removendo style inline e usando bg-black (Tailwind) para garantir o preto puro
+          // Fundo preto puro (#000000)
+          style={{ backgroundColor: '#000000' }}
         >
           <div className="p-6 pt-24 flex flex-col h-full">
             
