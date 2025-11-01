93b5c3146581c44f6b55f9d8964559e61ea939d3
[dyad] <acarlosdesouzajunior@gmail.com>
2025-11-01 19:13:12 -0300

    [dyad] Aplicando fundo preto puro (#000000) ao painel lateral móvel via estilo inline. - wrote 1 file(s)

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -140,9 +140,11 @@ const Navigation = ({ isOpen, setIsOpen }: NavigationProps) => {
 
         {/* 2. Side Panel (Painel Lateral) */}
         <div 
-          className={`lg:hidden fixed top-0 right-0 w-[70%] max-w-xs h-full border-l border-border shadow-2xl z-50 transform transition-transform duration-500 ease-in-out bg-black ${
+          className={`lg:hidden fixed top-0 right-0 w-[70%] max-w-xs h-full border-l border-border shadow-2xl z-50 transform transition-transform duration-500 ease-in-out ${
             isOpen ? 'translate-x-0' : 'translate-x-full'
           }`}
+          // Aplicando preto puro (#000000) via estilo inline
+          style={{ backgroundColor: '#000000' }}
         >
           <div className="p-6 pt-24 flex flex-col h-full">
             
