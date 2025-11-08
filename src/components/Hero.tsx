2792ddfb57cfd2427e98de003d1d70c74e9cb2ac
[dyad] <acarlosdesouzajunior@gmail.com>
2025-11-08 10:44:53 -0300

    [dyad] Ajustando a seção Hero para centralizar o conteúdo verticalmente e garantir que a altura da seção se adapte a dispositivos menores. - wrote 1 file(s)

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -21,7 +21,7 @@ const Hero = () => {
   }, []);
 
   return (
-    <section className="relative h-screen flex items-center justify-center overflow-hidden"> {/* Alterado min-h-screen para h-screen e removido py-24 */}
+    <section className="relative min-h-screen flex items-center justify-center overflow-hidden py-24 md:py-32"> {/* Alterado h-screen para min-h-screen e adicionado padding vertical */}
       <HeroCarousel />
       <FloatingParticles />
       
